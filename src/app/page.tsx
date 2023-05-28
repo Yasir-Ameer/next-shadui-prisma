d36abae2d03f048a93df4f67533eb5a45289227f
Yasir Ali <yasir@localhost.localdomain>
2023-05-28 15:26:46 +0500

    remove symbols

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,12 @@
 'use client'
 
 import { CustomButton } from '@/components/CustomButton'
+import Link from 'next/link'
 
 export default function Home() {
   return (
-    <div className='p-4'>
-           <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-        The Joke Tax Chronicles
-      </h1>
-      <p className="leading-7 [&:not(:first-child)]:mt-6">
-        Once upon a time
-      </p>
-      <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-        The Kings Plan
-      </h2>
-      <p className="leading-7 mb-6 [&:not(:first-child)]:mt-6">
-        The king thought long and hard, and finally came up with{" "}
-        <a
-          href="#"
-          className="font-medium text-primary underline underline-offset-4"
-        >
-          a brilliant plan
-        </a>
-        : he would tax the jokes in the kingdom.
-      </p>
-      <CustomButton />
-    </div>
+    <>
+      <Link href="/users">Users</Link>
+    </>
   )
 }
