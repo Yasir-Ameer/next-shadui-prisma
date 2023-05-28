5c479b9108b6d47bd9bd7077826c23eda215370a
Yasir Ali <yasir@localhost.localdomain>
2023-05-28 15:23:06 +0500

    Remove the special characters

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,12 +9,10 @@ export default function Home() {
         The Joke Tax Chronicles
       </h1>
       <p className="leading-7 [&:not(:first-child)]:mt-6">
-        Once upon a time, in a far-off land, there was a very lazy king who
-        spent all day lounging on his throne. One day, his advisors came to him
-        with a problem: the kingdom was running out of money.
+        Once upon a time
       </p>
       <h2 className="mt-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-        The King's Plan
+        The Kings Plan
       </h2>
       <p className="leading-7 mb-6 [&:not(:first-child)]:mt-6">
         The king thought long and hard, and finally came up with{" "}
